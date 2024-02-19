82d10cc438e66429695b869e8f6dbfe64411ebdb
asare-21 <joasare019@gmail.com>
2024-02-19 21:02:01 +0000

    written cache and schemas

--- server/app.js
+++ server/app.js
@@ -4,11 +4,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var app = express(); // gRPC
-var grpc = require('@grpc/grpc-js');
-var PROTO_PATH = __dirname + '../protos/protos/task.proto'; // gRPC
-var protoLoader = require('@grpc/proto-loader'); // gRPC
-var Server = new grpc.Server(); // gRPC
+var app = express(); 
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,19 +16,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
-// Suggested options for similarity to existing grpc.load behavior
-var packageDefinition = protoLoader.loadSync(
-    PROTO_PATH,
-    {
-        keepCase: true,
-        longs: String,
-        enums: String,
-        defaults: true,
-        oneofs: true
-    });
-var taskDescriptor = grpc.loadPackageDefinition(packageDefinition);
-// The taskDescriptor object has the full package hierarchy
-var routeguide = taskDescriptor.routeguide;
+
 
 
 
---
+++ server/task-service.js
@@ -0,0 +1,43 @@
+var grpc = require('@grpc/grpc-js');
+console.log(__dirname)
+var PROTO_PATH = '../protos/protos/task.proto'; // gRPC
+var protoLoader = require('@grpc/proto-loader'); // gRPC
+var server = new grpc.Server(); // gRPC
+
+
+// Suggested options for similarity to existing grpc.load behavior
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    });
+var taskDescriptor = grpc.loadPackageDefinition(packageDefinition);
+// The taskDescriptor object has the full package hierarchy
+var routeguide = taskDescriptor.routeguide;
+
+function getTaskList() { }
+function addTask() { }
+function deleteTask() { }
+function updateTask() { }
+
+function sayHello(call, callback) {
+    callback(null, { message: 'Hello ' + call.request.name });
+}
+
+function sayHelloAgain(call, callback) {
+    callback(null, { message: 'Hello again, ' + call.request.name });
+}
+
+function main() {
+    server.addService(taskDescriptor.Greeter.service,
+        { sayHello: sayHello, sayHelloAgain: sayHelloAgain });
+    server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+        server.start();
+    });
+}
+
+main();
